01d94309d7211a211c38a095339e8895e6ab0d1a
RamiRbehi <ramirbehi92@gmail.com>
2022-11-05 19:38:36 +0100

    finish with front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import Cart from "./Pages/Cart";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import ProductList from "./Pages/ProductList";
+import Products from "./Pages/Products";
+import Register from "./Pages/Register";
 
 const App = () => {
   return <Home/>;
--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 import {categories} from "../Data"
+import { mobile } from "../Responsive"
 import CategoryItem from "./CategoryItem"
 
 const Container =styled.div`
     display: flex;
     padding: 20px;
     justify-content: space-between;
+    ${mobile({padding:"0", flexDirection: "column"})}
 `
 
 const Categories = () => {
